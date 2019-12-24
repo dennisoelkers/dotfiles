7f0e579024d3b1bc62294004dc2fa0f279aad395
Dennis Oelkers <dennis@lauschmusik.de>
2019-12-24 11:06:41 +0100

    Adding lock script.

---
+++ bin/i3lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+icon="$HOME/.config/i3/lock.png"
+img="$HOME/.cache/i3lock.png"
+
+# Take a screenshot for our background
+scrot $img
+# Pixelate the background
+convert $img -scale 10% -scale 1000% $img
+# Finally run i3lock itself
+i3lock -i $img
+
