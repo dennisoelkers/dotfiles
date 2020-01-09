561005da2e7aaef1b69a97fa5eadc0a5242680e9
Dennis Oelkers <dennis@graylog.com>
2020-01-09 20:00:03 +0100

    Using overwrite option.

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -3,7 +3,7 @@
 img="$HOME/.cache/i3lock.png"
 
 # Take a screenshot for our background
-scrot $img
+scrot -o $img
 # Pixelate the background
 convert $img -scale 10% -scale 1000% $img
 # Finally run i3lock itself
